6a5c488c1961a54588532676f7748dd56c9ee3e1
msintaha <msintaha94@gmail.com>
2020-03-07 14:58:32 +0600

    Add sinon assertion example

--- src/pages/Home/Home.spec.js
+++ src/pages/Home/Home.spec.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Home from './index';
-import { expect } from 'chai';
+import Service from '../../services/Service';
+import * as should from 'should';
 
 describe("Home", () => {
-  it("should render my component", () => {
-    const wrapper = mount(<Home />);
+  let sandbox, wrapper;
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(Service, 'list').resolves([
+      'apple',
+      'oranges'
+    ])
+  })
+
+  afterEach(() => {
+    sandbox.restore();
+    wrapper && wrapper.unmount();
+  })
+
+  function render() {
+    wrapper = mount(<Home />);
+  }
+
+  it("should render Home component", () => {
+    render()
     expect(wrapper.find('.home').text()).to.contain('Latte');
+  });
+
+  it("should call Service list", () => {
+    render()
+
+    wrapper.update();
+    Service.list.should.have.been.called;
   });
 });
--- src/test/helper.js
+++ src/test/helper.js
@@ -1,6 +1,9 @@
 import { expect } from 'chai';
 import { configure } from 'enzyme';
+import sinon from 'sinon';
+
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 global.expect = expect;
+global.sinon = sinon;
