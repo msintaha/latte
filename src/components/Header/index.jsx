ae7ff6b1d5c5ac6aae1e403ef0a4f0aa67b2d43e
Mifta Sintaha <msintaha94@gmail.com>
2020-03-17 18:46:30 +0600

    Fix navbar classes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,16 +4,11 @@ import logo from '../../assets/images/latte.png';
 function Header() {
   return (
     <div className="header">
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <a className="navbar-brand" href="/">
-              <img alt="Latte" height="22" src={logo} />
-            </a>
-            <p className="navbar-text">Latte</p>
-          </div>
-        </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <img src={logo} height="22" />
+        <a className="navbar-brand" href="/">Latte</a>
       </nav>
+      <br />
     </div>
   );
 }
